0edc949d3884d11e2ba88e6bc5cb26d811487c57
Icaro Silva <ikaromarlon@gmail.com>
2024-02-11 19:21:13 +0100

    chore: add user model to prisma and run migration

---
+++ prisma/migrations/20240211171347_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
