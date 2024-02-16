201a22aaaf1ab258eae477526830de999a0fcc70
Icaro Silva <ikaromarlon@gmail.com>
2024-02-16 19:15:42 +0100

    chore: change integration tests config

--- tests/jest.config.integration.ts
+++ tests/jest.config.integration.ts
@@ -3,11 +3,6 @@ import globalConfig from '../jest.config'
 
 const config: Config = {
   ...globalConfig,
-  collectCoverage: true,
-  coverageDirectory: '<rootDir>/coverage/integration',
-  collectCoverageFrom: [
-    '<rootDir>/src/modules/**/*.ts'
-  ],
   roots: [
     '<rootDir>/src',
     '<rootDir>/tests/integration'
