4fb57c57da277d911a40790178dd1c302b239367
Icaro Silva <ikaromarlon@gmail.com>
2024-02-16 16:20:45 +0100

    feat: add updateUser service

---
+++ src/modules/users/updateUser/service.ts
@@ -0,0 +1,22 @@
+import type { UsersRepository } from '../../../repositories/UsersRepository'
+import type { User, UpdateUserData } from '../User.entity'
+
+export default class UpdateUserService {
+  constructor (
+    private readonly usersRepository: UsersRepository
+  ) {}
+
+  public async execute (id: string, data: UpdateUserData): Promise<User> {
+    if (data.email) {
+      const existingUser = await this.usersRepository.findOne({ email: data.email })
+
+      if (existingUser && existingUser.id !== id) {
+        throw new Error('There is another user using the email provided')
+      }
+    }
+
+    const user = await this.usersRepository.update(id, data)
+
+    return user
+  }
+}
